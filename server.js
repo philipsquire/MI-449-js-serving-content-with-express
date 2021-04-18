fd60666da155e5aff9d52c0861cb3c2cbd70b85e
Philip Squire <squireph@msu.edu>
2021-04-18 15:37:28 -0400

    Fixed validation error, added CSS, adjusted page generation

--- server.js
+++ server.js
@@ -15,20 +15,23 @@ app.get('/', function (request, response) {
 })
 
 app.get('/jojo', function (request, response) {
-  response.render('pages/jojo', {
-    pages: pages
+  response.render('pages/anime', {
+    pages: pages,
+    data: pages[1]
   })
 })
 
 app.get('/baccano', function (request, response) {
-  response.render('pages/baccano', {
-    pages: pages
+  response.render('pages/anime', {
+    pages: pages,
+    data: pages[2]
   })
 })
 
 app.get('/kino', function (request, response) {
-  response.render('pages/kino', {
-    pages: pages
+  response.render('pages/anime', {
+    pages: pages,
+    data: pages[3]
   })
 })
 
@@ -56,7 +59,7 @@ createPage({
 createPage({
   name: 'Baccano',
   content: 'From the Italian for "ruckus", you get an anime that could be very much be described as one. Prohibition-era gangsters mixed with immortality and presented out of chronological order. What\'s not to love? The only crime that matters is that the subsequent novels in the series didn\'t get animated.',
-  picture: '<img source = "/images/baccano.png" alt="Cast of Baccano">',
+  picture: '<img src = "/images/baccano.png" alt="Cast of Baccano">',
   outlink: '<a href = "https://anidb.net/anime/4897">AniDB</a>',
   link: '<a href = "/baccano">Baccano</a>'
 })
@@ -64,7 +67,7 @@ createPage({
 createPage({
   name: 'Kino\'s Journey',
   content: 'This one is special, as it has a specific format that works wonders. Each episode, the titular Kino and her talking motorcycle, Hermes, go to a different country (city-states) based around a certain theme or concept. They are only allowed to stay for a couple of days, and then they must leave. It\'s  a beautiful anthology series that should not be missed. Just make sure you watch the 2003 version and not the new one, as they left it up to the fans to pick their favourite stories, and it did not end well.',
-  picture: '<img source = "/images/kino.jpg" alt="Kino, the main character">',
+  picture: '<img src = "/images/kino.jpg" alt="Kino, the main character">',
   outlink: '<a href = "https://anidb.net/anime/575">AniDB</a>',
   link: '<a href = "/kino">Kino</a>'
 })
